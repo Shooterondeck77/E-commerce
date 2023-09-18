8bc9a3bd25955e05dffe6f34825f850be7ff8b02
Shooterondeck77 <timothytaffur@gmail.com>
2023-09-17 21:38:29 -0400

    Added columns to all

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -8,55 +8,6 @@ CREATE DATABASE ecommerce_db;
 
 USE ecommerce_db;
 
-CREATE TABLE category
- (
-
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-  category_name CHARACTER_LENGTH(30) NOT NULL
-);
-
-
-CREATE TABLE product
- (
-
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-  product_name CHARACTER_LENGTH(30) NOT NULL,
-
-  price DECIMAL(5,2) NOT NULL ISNUMERIC() ,
-
-  stock INT NOT NULL VARCHAR(10) ISNUMERIC(),
-
-  category_id INT,
-  FOREIGN KEY (category_id) REFERENCES category(id)
-);
-
-CREATE TABLE Tag
-
-(
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-tag_name CHARACTER_LENGTH(30),
-);
-
-
-CREATE TABLE ProductTag
-(
-id INT NOT NULL AUTO_INCREMENT primary key(ProductTag),
-
-product_id INT, 
-FOREIGN KEY (product_id) REFERENCES product(id),
-
-tag_id INT,
-FOREIGN KEY (tag_id) REFERENCES Tag(id),
-
-);
-
-
-
-
-
 
 
 
